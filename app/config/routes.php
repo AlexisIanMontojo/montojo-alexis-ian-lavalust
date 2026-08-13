23c048850157dceebd65e14eeb53ce16e921b6ed
Alexis Montojo <alexisian@gmail.com>
2026-08-13 14:46:02 +0800

    Updated Lavalust Landing Page

--- app/config/routes.php
+++ app/config/routes.php
@@ -45,4 +45,3 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 /** @var object $router **/
 
 $router->get('/', 'Welcome::index');
-$router->get('/members', 'Welcome::names');
--- app/controllers/Welcome.php
+++ app/controllers/Welcome.php
@@ -5,8 +5,5 @@ class Welcome extends Controller {
 	public function index() {
 		$this->call->view('welcome_page');
 	}
-	public function names() {
-		$this->call->view('Alex');
-	}
 }
 ?>
